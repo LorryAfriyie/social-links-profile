c068f42fbd242889a82e31cddc2167790c6d6a03
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-02-29 04:58:59 +0200

    vite.config file changes
    Refactoring error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://lorryafriyie.github.io/social-links-profile/"
-  },
+  base: "https://lorryafriyie.github.io/social-links-profile/",
 });

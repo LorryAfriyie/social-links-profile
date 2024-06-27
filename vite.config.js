45bbede27edf596a700191658ea53d28933fa583
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-06-27 03:50:08 +0200

    vite config adjustment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "https://lorryafriyie.github.io/social-links-profile/",
+  server: {
+    port: 3002,
+  },
 });
